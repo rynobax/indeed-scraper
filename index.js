86cd651410bfd5d337dd1e05b13b8a09cbbc0288
Ryan Baxley <rynobax@users.noreply.github.com>
2020-03-01 22:48:03 -0500

    Housekeeping (#12)
    * Install prettier
    
    * Add np

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-'use strict';
-const indeedScraper = require('./lib/scraper.js');
+"use strict";
+const indeedScraper = require("./lib/scraper.js");
 
-module.exports.query = function(queryObject){
+module.exports.query = function(queryObject) {
   return indeedScraper.query(queryObject);
-}
+};
